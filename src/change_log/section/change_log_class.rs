8aff5749ac52f33c4c11887eb2bc01f12174b1b7
Jeremiah Russell <jerry@jrussell.ie>
2025-08-26 11:48:30 +0100

    ✨ feat(change_log): add changelog classification enum
    - introduce ChangeLogClass enum to classify commit types for changelogs
    - support various classifications including Added, Fixed, and Security

---
+++ src/change_log/section/change_log_class.rs
@@ -0,0 +1,40 @@
+//! Classify the kinds of Conventional Commit for display in a changelog
+//!
+//! The classifications are based on Keep a Changelog with an additional `Misc` classification for unknown classifications and `Unclassified` for when the there is no kind identified. Multiple kinds may be given the same classification for the purpose of the changelog.
+
+#[derive(Debug)]
+pub(crate) enum ChangeLogClass {
+    Added,
+    Fixed,
+    Changed,
+    Security,
+    Build,
+    Documentation,
+    Chore,
+    CI,
+    Test,
+    Deprecated,
+    Removed,
+    Misc,
+    Unclassified,
+}
+
+impl ChangeLogClass {
+    pub(crate) fn new(kind: &str) -> Self {
+        match kind.to_lowercase().as_str() {
+            "feat" => ChangeLogClass::Added,
+            "fix" => ChangeLogClass::Fixed,
+            "refactor" => ChangeLogClass::Changed,
+            "security" | "dependency" => ChangeLogClass::Security,
+            "build" => ChangeLogClass::Build,
+            "doc" | "docs" => ChangeLogClass::Documentation,
+            "chore" => ChangeLogClass::Chore,
+            "ci" => ChangeLogClass::CI,
+            "test" => ChangeLogClass::Test,
+            "deprecated" => ChangeLogClass::Deprecated,
+            "removed" => ChangeLogClass::Removed,
+            "misc" => ChangeLogClass::Misc,
+            _ => ChangeLogClass::Unclassified,
+        }
+    }
+}
