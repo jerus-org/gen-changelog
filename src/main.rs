09b2f92ea847fabeca45d25de186bf6b49b9035b
Jeremiah Russell <jerry@jrussell.ie>
2025-08-26 11:18:41 +0100

    ✨ feat(main): integrate changelog generation
    - add PathBuf and ChangeLog imports for changelog creation
    - implement changelog generation and printing in main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::path::PathBuf;
+
+use gen_changelog::ChangeLog;
+
 fn main() {
-    println!("Hello, world!");
+    let repo_dir = PathBuf::new().join(".");
+    let mut change_log = ChangeLog::new(&repo_dir);
+
+    change_log.build().unwrap().print();
 }
