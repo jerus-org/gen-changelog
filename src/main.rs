7a444605a32d68b027862ba7d2503744b0afd688
Jeremiah Russell <jerry@jrussell.ie>
2025-08-27 08:40:08 +0100

    ✨ feat(logging): add logging functionality to main
    - introduce get_logging function with log level filter
    - initialize logging in main function for better traceability

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,21 @@ use std::path::PathBuf;
 use gen_changelog::ChangeLog;
 
 fn main() {
+    let mut logging = get_logging(log::LevelFilter::Debug);
+    logging.init();
+
     let repo_dir = PathBuf::new().join(".");
     let mut change_log = ChangeLog::new(&repo_dir).unwrap();
 
     change_log.build().unwrap().print();
+}
+
+fn get_logging(level: log::LevelFilter) -> env_logger::Builder {
+    let mut builder = env_logger::Builder::new();
+
+    builder.filter(None, level);
+
+    builder.format_timestamp_secs().format_module_path(false);
+
+    builder
 }
