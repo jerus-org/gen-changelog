8c4deb75cff7c6cf2fd982f900f7770874319553
Jeremiah Russell <jerry@jrussell.ie>
2025-09-01 17:28:39 +0100

    ✨ feat(config): implement heading management feature
    - add HeadingMgmt trait for managing group headings
    - support adding, removing, and maintaining miscellaneous group
    - ensure idempotence in group addition
    - include tests for functionality validation

---
+++ src/config/heading_mgmt.rs
@@ -0,0 +1,106 @@
+use std::collections::BTreeMap;
+
+pub(crate) trait HeadingMgmt {
+    fn add_group(&mut self, group: &str) -> &mut Self;
+    fn add_miscellaneous(&mut self) -> &mut Self {
+        self
+    }
+    fn remove_miscellaneous(&mut self) -> &mut Self {
+        self
+    }
+}
+
+impl HeadingMgmt for BTreeMap<u8, String> {
+    fn add_group(&mut self, group: &str) -> &mut Self {
+        let i = self.len() as u8;
+        if i == u8::MAX {
+            log::warn!("maximum number of groups created ({})", u8::MAX);
+            self
+        } else if self.iter().any(|g| g.1 == &group.to_string()) {
+            self
+        } else {
+            if self.iter().any(|g| g.1 == &"Miscellaneous".to_string()) && group != "Miscellaneous"
+            {
+                self.insert(i - 1, group.to_string());
+                self.insert(i, "Miscellaneous".to_string());
+            } else {
+                self.insert(i, group.to_string());
+            }
+
+            self
+        }
+    }
+
+    fn add_miscellaneous(&mut self) -> &mut Self {
+        if self.iter().any(|g| g.1 == "Miscellaneous") {
+            self
+        } else {
+            self.add_group("Miscellaneous");
+            self
+        }
+    }
+
+    fn remove_miscellaneous(&mut self) -> &mut Self {
+        if self.iter().any(|g| g.1 == "Miscellaneous") {
+            let key = self.len() as u8 - 1;
+            self.remove(&key);
+            self
+        } else {
+            self
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::BTreeMap;
+
+    use super::HeadingMgmt;
+
+    #[test]
+    fn test_keep_misc_last() {
+        let mut headings = BTreeMap::new();
+        headings.add_group("added");
+        assert!(headings.first_entry().is_some());
+        headings.add_group("Miscellaneous");
+        assert!(headings.last_entry().is_some());
+        let last = headings.last_entry().unwrap();
+        let last = last.get();
+        assert_eq!(last, &"Miscellaneous".to_string());
+        headings.add_group("changed");
+        assert!(headings.last_entry().is_some());
+        let last = headings.last_entry().unwrap();
+        let last = last.get();
+        assert_eq!(last, &"Miscellaneous".to_string());
+    }
+
+    #[test]
+    fn test_add_and_remove_miscellaneous() {
+        let mut headings = BTreeMap::new();
+        headings.add_group("added");
+        assert!(headings.first_entry().is_some());
+        headings.add_miscellaneous();
+        assert_eq!(headings.len(), 2);
+        assert!(headings.last_entry().is_some());
+        let last = headings.last_entry().unwrap();
+        let last = last.get();
+        assert_eq!(last, &"Miscellaneous".to_string());
+        headings.remove_miscellaneous();
+        assert_eq!(headings.len(), 1);
+        assert!(headings.last_entry().is_some());
+        let last = headings.last_entry().unwrap();
+        let last = last.get();
+        assert_ne!(last, &"Miscellaneous".to_string());
+    }
+
+    #[test]
+    fn test_adding_group_idempotence() {
+        let mut headings = BTreeMap::new();
+        headings.add_group("added");
+        assert_eq!(headings.len(), 1);
+        assert!(headings.last_entry().is_some());
+        headings.add_group("added");
+        assert_eq!(headings.len(), 1);
+        assert!(headings.last_entry().is_some());
+    }
+}
