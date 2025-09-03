05c9e2cf572944e54a0604164849280aa9cc18dc
Jeremiah Russell <jerry@jrussell.ie>
2025-09-03 17:54:13 +0100

    ✨ feat(change_log): add metadata support to Header struct
    - introduce DEFAULT_METADATA constant for default metadata value
    - add metadata field to Header struct with default implementation
    - implement set_metadata method to allow metadata customization
    - update Display implementation to include metadata in output

--- src/change_log/header.rs
+++ src/change_log/header.rs
@@ -1,7 +1,7 @@
 use std::fmt::Display;
 
+const DEFAULT_METADATA: &str = "<!-- LTex: Enabled=false -->";
 const DEFAULT_TITLE: &str = "Changelog";
-
 const DEFAULT_PARAGRAPHS: [&str; 2] = [
     "All notable changes to this project will be documented in this file.",
     "The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).",
@@ -9,33 +9,50 @@ const DEFAULT_PARAGRAPHS: [&str; 2] = [
 
 #[derive(Debug, Clone)]
 pub(crate) struct Header {
+    metadata: String,
     title: String,
     paragraphs: Vec<String>,
 }
 
 impl Default for Header {
     fn default() -> Self {
+        let metadata = String::from(DEFAULT_METADATA);
         let title = String::from(DEFAULT_TITLE);
         let mut paragraphs = Vec::new();
         for paragraph in DEFAULT_PARAGRAPHS {
             paragraphs.push(paragraph.to_string());
         }
-        Self { title, paragraphs }
+        Self {
+            metadata,
+            title,
+            paragraphs,
+        }
     }
 }
+
 impl Header {
     pub(crate) fn new(title: &str, paras: &[&str]) -> Self {
+        let metadata = String::from(DEFAULT_METADATA);
         let title = String::from(title);
         let mut paragraphs = Vec::new();
         for paragraph in paras {
             paragraphs.push(paragraph.to_string());
         }
-        Self { title, paragraphs }
+        Self {
+            metadata,
+            title,
+            paragraphs,
+        }
+    }
+
+    pub(crate) fn set_metadata(&mut self, value: &str) {
+        self.metadata = value.to_string();
     }
 }
 
 impl Display for Header {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{}", self.metadata)?;
         writeln!(f, "# {}", self.title)?;
         for para in self.paragraphs.iter() {
             writeln!(f)?;
