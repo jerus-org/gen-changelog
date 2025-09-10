c798cf9c4ffbc65aba0dc75bad64e37fd4577d94
Jeremiah Russell <jerry@jrussell.ie>
2025-09-10 17:50:41 +0100

    ✨ feat(lib): export ChangeLogBuilder
    - add ChangeLogBuilder to public exports for improved API usability

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ mod change_log_config;
 mod error;
 
 pub use change_log::ChangeLog;
+pub use change_log::ChangeLogBuilder;
 pub use change_log_config::ChangeLogConfig;
 pub use change_log_config::DisplaySections;
 pub use error::Error;
