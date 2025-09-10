6056d834a1ac9d6f63e7e1bb3d862b95d9750b9d
Jeremiah Russell <jerry@jrussell.ie>
2025-09-10 15:50:06 +0100

    ✨ feat(error): add custom error handling with thiserror
    - implement error enum for gen-changelog crate using thiserror
    - define specific error variants for better error management

---
+++ src/error.rs
@@ -0,0 +1,30 @@
+use thiserror::Error;
+
+/// Error messages for the gen-changelog crate
+#[derive(Debug, Error)]
+pub enum Error {
+    /// url not found
+    #[error("url not found")]
+    UrlNotFound,
+    /// capture groups not found
+    #[error("capture groups not found")]
+    CapturesNotFound,
+    /// owner not found in capture group
+    #[error("owner not found in capture group")]
+    OwnerNotFound,
+    /// repo not found in capture group
+    #[error("repo not found in capture group")]
+    RepoNotFound,
+    /// Error from the git2 crate
+    #[error("Git2 says: {0}")]
+    Git2Error(#[from] git2::Error),
+    /// Error from the std io
+    #[error("io error: {0}")]
+    IOError(#[from] std::io::Error),
+    /// Error from the toml serializer
+    #[error("toml serializer error: {0}")]
+    TomlSerError(#[from] toml::ser::Error),
+    // /// Error from the regex crate
+    // #[error("Regex says: {0}")]
+    // RegexError(#[from] regex::Error),
+}
