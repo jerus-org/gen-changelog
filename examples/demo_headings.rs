46453b4cb071cb390e92c251bd957ac293ec7afa
Jeremiah Russell <jerry@jrussell.ie>
2025-09-10 14:55:51 +0100

    ✨ feat(examples): add demo for ChangeLogConfig usage
    - create demo_headings.rs to demonstrate default ChangeLogConfig serialization
    - provide basic example for developers to understand config usage

---
+++ examples/demo_headings.rs
@@ -0,0 +1,8 @@
+use gen_changelog::ChangeLogConfig;
+
+fn main() {
+    let _config = ChangeLogConfig::default();
+
+    println!("Default ChangeLogConfig serialized to TOML:");
+    // let _ = config.save();
+}
