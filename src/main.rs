ff23e951e1a35c05013160690b65921f113cb70c
Jeremiah Russell <jerry@jrussell.ie>
2025-09-12 12:24:49 +0100

    ♻️ refactor(main): streamline CLI structure
    - remove unused imports and redundant code
    - add generate_cli module for changelog generation
    - simplify run function by removing default changelog handling

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,26 @@
-use std::{error::Error, path::PathBuf};
+use std::error::Error;
 
 mod config_cli;
+mod generate_cli;
 
 use clap::{Parser, Subcommand};
-use gen_changelog::{ChangeLog, ChangeLogConfig, DisplaySections};
-use git2::Repository;
 
-use crate::config_cli::ConfigCli;
+use crate::{config_cli::ConfigCli, generate_cli::GenerateCli};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
     #[clap(flatten)]
     logging: clap_verbosity_flag::Verbosity,
-    /// The next version number for unreleased changes
-    #[arg(short, long)]
-    next_version: Option<String>,
-    /// The number of level 2 headings to show in the changelog
-    #[arg(short, long)]
-    sections: Option<u8>,
-    /// The number of level 2 headings to show in the changelog
-    #[arg(short, long)]
-    config_file: Option<String>,
     #[command(subcommand)]
     command: Option<Commands>,
 }
 
 #[derive(Subcommand, Debug)]
 enum Commands {
+    /// Generate Changelog
+    #[clap(name = "generate")]
+    Generate(GenerateCli),
     /// Configuration management
     #[clap(name = "config")]
     Configuration(ConfigCli),
@@ -57,52 +50,11 @@ fn main() {
 fn run(args: Cli) -> Result<(), gen_changelog::Error> {
     if let Some(cmds) = args.command {
         match cmds {
+            Commands::Generate(generate_cli) => generate_cli.run()?,
             Commands::Configuration(config_cli) => config_cli.run()?,
         }
-    } else {
-        let repo_dir = PathBuf::new().join(".");
-        let repository = Repository::open(&repo_dir)
-            .unwrap_or_else(|_| panic!("unable to open the repository at {}", &repo_dir.display()));
-
-        let sections = if let Some(n) = args.sections {
-            if n == 1 {
-                DisplaySections::One
-            } else {
-                DisplaySections::Custom(n)
-            }
-        } else {
-            DisplaySections::All
-        };
-
-        let mut config = if let Some(cfg) = args.config_file {
-            ChangeLogConfig::from_file(cfg)?
-        } else {
-            ChangeLogConfig::from_file_or_default()?
-        };
-        log::trace!("initial config to build on: {config:?}");
-
-        config.publish_group("Security");
-        config.set_display_sections(sections);
-
-        log::trace!("{config:#?}");
-
-        let mut change_log = default_changelog_build(&repository, config);
-        change_log.update_unreleased_to_next_version(args.next_version.as_ref());
-
-        let _ = change_log.save();
-        println!("{change_log}");
     }
     Ok(())
-}
-
-fn default_changelog_build(repository: &Repository, config: ChangeLogConfig) -> ChangeLog {
-    let mut change_log_builder = ChangeLog::builder();
-
-    change_log_builder
-        .with_config(config)
-        .with_repository(repository)
-        .unwrap()
-        .build()
 }
 
 fn get_logging(level: log::LevelFilter) -> env_logger::Builder {
