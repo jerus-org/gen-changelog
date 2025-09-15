8eed42c377c8aba5ff72b61c5ccf1e89358b5ec0
Jeremiah Russell <jerry@jrussell.ie>
2025-09-15 06:39:40 +0100

    💄 style(shell): add shellcheck directive for appending
    - add shellcheck directive to disable SC2129 warning when appending to README.md

--- release-hook.sh
+++ release-hook.sh
@@ -2,6 +2,7 @@
 
 # Build an updated README
 cat docs/readme/head.md > README.md
+# shellcheck disable=SC2129
 cat docs/lib.md >> README.md
 cat docs/main.md >> README.md
 cat docs/readme/tail.md >> README.md   
