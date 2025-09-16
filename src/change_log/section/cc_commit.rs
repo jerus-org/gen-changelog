b298ab45e76f83b2cb604ec61010cfaa1be63a62
Jeremiah Russell <jerry@jrussell.ie>
2025-09-16 12:55:11 +0100

    ✅ test(cc_commit): use test_utils for logger setup
    - replace inline test logger setup with `get_test_logger` from `test_utils`
    - improve code maintainability and reuse by centralizing logger setup

--- src/change_log/section/cc_commit.rs
+++ src/change_log/section/cc_commit.rs
@@ -124,17 +124,10 @@ impl std::fmt::Display for ConvCommit {
 #[cfg(test)]
 mod tests {
 
-    use log::LevelFilter;
+    use crate::test_utils::get_test_logger;
     use rstest::rstest;
 
     use super::*;
-
-    fn get_test_logger() {
-        let mut builder = env_logger::Builder::new();
-        builder.filter(None, LevelFilter::Debug);
-        builder.format_timestamp_secs().format_module_path(false);
-        let _ = builder.try_init();
-    }
 
     #[test]
     fn test_cmt_summary_parse() {
