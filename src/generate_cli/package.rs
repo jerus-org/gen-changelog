bd1928da082daa54b9fcc6fe89720851a4da385a
Jeremiah Russell <jerry@jrussell.ie>
2025-09-26 07:30:36 +0100

    🐛 fix(generate_cli): add debug logging for package retrieval
    - add debug logs to trace package retrieval and insertion processes
    - enhance visibility into Cargo.toml parsing and package detection steps

--- src/generate_cli/package.rs
+++ src/generate_cli/package.rs
@@ -30,6 +30,7 @@ struct Package {
 }
 
 pub(crate) fn get_packages(root: &Path) -> Result<HashMap<String, PathBuf>, Error> {
+    log::debug!("getting the rust packages in the repository");
     let mut packages = HashMap::new();
 
     if let Some(root_cargo) = insert_package(root, &mut packages) {
@@ -53,11 +54,22 @@ pub(crate) fn get_packages(root: &Path) -> Result<HashMap<String, PathBuf>, Erro
 fn insert_package(root: &Path, map: &mut HashMap<String, PathBuf>) -> Option<Cargo> {
     let mut ret = None;
     let cargo_file = root.join("Cargo.toml");
+    log::debug!("inserting package from `{}`", cargo_file.display());
     let cargo_string = read_to_string(cargo_file).expect("no `Cargo.toml` found");
+    log::debug!("read to string:\n{cargo_string:#?}");
     if let Ok(cargo) = toml::from_str::<Cargo>(&cargo_string) {
+        log::debug!("toml read from the string");
         if let Some(package) = cargo.package() {
+            log::debug!("found the package `{package:?}`");
             if let Some(name) = package.name {
+                log::debug!("found the name in the package `{name}`");
                 map.insert(name, root.to_path_buf());
+                ret = Some(cargo);
+            }
+        } else {
+            log::debug!("no package found");
+            if cargo.workspace.is_some() {
+                log::debug!("workspace Cargo.toml");
                 ret = Some(cargo);
             }
         }
